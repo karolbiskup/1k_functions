f9eccb823a611b088e8b89274a647f41ca03d923
karolbiskup <karolbiskup1234@gmail.com>
2024-05-17 11:01:55 +0200

    basic functions area

---
+++ 1k_functions.py
@@ -0,0 +1,24 @@
+def area_square(side):
+    return side*side
+
+
+def area_rectangle(a, b):
+    return a*b
+
+
+def area_triangle(a, h):
+    return (a*h) / 2
+
+
+def area_circle(r):
+    return 3.14*r*r
+
+
+def area_trapeze(a, b, h):
+    return (a+b)*h/2
+
+
+print(area_trapeze(2, 2, 2))
+
+
+
